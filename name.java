6bbab3386c178c5ef978ce6fa4ae0483fbdb7a65
Latheesh9865 <113575192+Latheesh9865@users.noreply.github.com>
2023-10-27 20:30:53 +0530

    Add files via upload

---
+++ name.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+class name
+
+{
+  public static void main(String args[])
+  {
+    Scanner s=new Scanner(System.in);
+     String name;
+    System.out.println("may i know your name");
+
+     name=s.next();
+      System.out.println("welcome mr/ms"+name);
+}}
