64459d3ebb54f920d0d3f2db3fe5a45563ebcf0c
Latheesh9865 <113575192+Latheesh9865@users.noreply.github.com>
2023-12-19 13:13:08 +0530

    Add files via upload

--- gcdlcm.java
+++ gcdlcm.java
@@ -1,17 +1,28 @@
 import java.util.*;
-class gcdlcm
+class GcdLcm_3
 {
- public static void main(String args[])
-{
- int n1,n2,i;
-   Scanner s=new Scanner(System.in);
-    System.out.println("enter the 2 numbers");
-   n1=s.nextInt();
-   n2=s.nextInt();
-   for(i=2;i<=n1&&i<=n2;i++)
-{
-if(n1%i==0&&n2%i==0)
-{
- 
-System.out.println(i);}}
-}}
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		int n1,n2,n,gcd=1,lcm,i,n3;
+		System.out.println("enter the n value: ");
+		n=s.nextInt();
+		System.out.println("enter n1: ");
+		n1=s.nextInt();
+		System.out.println("enter n2: ");
+		n2=s.nextInt();
+		System.out.println("enter n3: ");
+		n3=s.nextInt();
+		for(i=1;i<=n1 && i<=n2 && i<=n3;i++)
+		{
+			if(n1%i==0 && n2%i==0 && n3%i==0)
+			{
+
+				gcd=i;
+			}
+		}
+		lcm=(n1*n2*n3)/gcd;
+		System.out.println("gcd is: "+gcd);
+		System.out.println("lcm is: "+lcm);
+	}
+}
